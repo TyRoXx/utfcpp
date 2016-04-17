59fe95bfa789c060edfa8b6e5925c9a1091b492c
TyRoXx <tyroxxxx@gmail.com>
2016-04-17 23:17:02 +0200

    uploaded to conan.io

---
+++ conanfile.py
@@ -0,0 +1,13 @@
+from conans import ConanFile
+import os
+
+class UtfCppConan(ConanFile):
+    name = "utfcpp"
+    version = "2.3.4"
+    generators = "cmake"
+    url="http://github.com/tyroxx/utfcpp"
+    license="Boost"
+    exports="source/*"
+
+    def package(self):
+        self.copy(pattern="*.h", dst="include", src="source", keep_path=True)
